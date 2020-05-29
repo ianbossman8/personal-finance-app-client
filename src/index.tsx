fe6d712adebdc1ecbb30a41e7d2ce4c9e36d1960
ian chan <ian035@hotmail.com>
2020-05-29 20:35:12 +0100

    setup:formatters stylint configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import styled, { ThemeProvider } from "styled-components";
-import { myTheme } from "./style/theme";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled, { ThemeProvider } from 'styled-components'
+import { theme } from './style/theme'
 
 const PageContainer = styled.div`
   color: ${(props) => props.theme.colors.main};
-`;
+`
 
 async function appStart() {
   ReactDOM.render(
-    <ThemeProvider theme={myTheme}>
+    <ThemeProvider theme={theme}>
       <PageContainer>Coming soon</PageContainer>
     </ThemeProvider>,
-    document.getElementById("root")
-  );
+    document.getElementById('root')
+  )
 }
 
-appStart();
+appStart()
