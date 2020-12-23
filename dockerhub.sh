8ea91fa9f0d413d94e7145c4dd7c3dec2e344cc1
ian chan <ian035@hotmail.com>
2020-12-23 20:01:44 +0000

    devops(docker):fix env var 3

--- dockerhub.sh
+++ dockerhub.sh
@@ -2,10 +2,9 @@
 echo "build starting after successful testing"
 echo "current job on branch $TRAVIS_BRANCH"
 echo "pull request $TRAVIS_PULL_REQUEST"
-echo "COGNITO_USER_POOL_ID $COGNITO_USER_POOL_ID"
 if [ "$TRAVIS_BRANCH" == "master" ] && [ $TRAVIS_PULL_REQUEST == false ]
 then
-  docker build --build-arg $COGNITO_USER_POOL_ID --build-arg $COGNITO_CLIENT_ID -t ian035/personal-finance-app-client .
+  docker build --build-arg COGNITO_USER_POOL_ID=$COGNITO_USER_POOL_ID --build-arg COGNITO_CLIENT_ID=$COGNITO_CLIENT_ID -t ian035/personal-finance-app-client .
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
   docker push ian035/personal-finance-app-client
 else
